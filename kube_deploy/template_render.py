10a7ef1c47be573b7db861d94a906cac239d51b1
Gautham Nambi <gautham.n@browserstack.com>
2021-10-25 23:53:35 +0530

    ansible updated for k8s deployment

--- kube_deploy/template_render.py
+++ kube_deploy/template_render.py
@@ -4,7 +4,10 @@ import sys
 import os
 
 if __name__ == "__main__":
+	variable_dir = os.path.dirname(__file__)
+	os.chdir(variable_dir)
 	values = yaml.load(open('variables.yaml'))
 	env = Environment(loader = FileSystemLoader(os.getcwd()), trim_blocks = True, lstrip_blocks = True)
+	
 	temp = env.get_template(sys.argv[1])
 	print(temp.render(values))
